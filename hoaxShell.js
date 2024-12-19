b7947e7f4790a9898e385e6d3873609cc41502fc
sanika <137403800+sanikaP16@users.noreply.github.com>
2024-12-19 18:36:21 +0530

    added ls,touch,exit commands

--- hoaxShell.js
+++ hoaxShell.js
@@ -1,6 +1,67 @@
-const command = 'hoaxShell / terminal';
+let continueToShell = true;
+const shellPrompt = 'hoaxShell ';
+let currentDirectory = '~/';
+const symbol = '% ';
+const file = [];
 
-while (true) {
-  const userCommand = prompt(command);
-  console.log("You entered ", userCommand);
+const executeCd = function (args) {
+  if (args.length === 0) {
+    return "Error : no dirctory specified.";
+  }
+
+  currentDirectory = currentDirectory.concat("/", args.join("/"));
+
+  return;
 }
+
+const executePwd = function () {
+  return currentDirectory;
+}
+
+const executeEcho = function (args) {
+  return args.join(" ");
+}
+
+const executeExit = function () {
+  continueToShell = false;
+  return "Thank you....\n";
+}
+
+const executeTouch = function (args) {
+  file.push(args);
+  return
+}
+
+const executeLs = function () {
+  return file.join("  ")
+}
+
+const runCommand = function (commandString) {
+  const [command, ...args] = commandString.split(" ");
+  const listOfCallbacks = [executeCd, executePwd, executeEcho,
+    executeExit, executeLs, executeTouch];
+  const listOfCommands = ['cd', 'pwd', 'echo', 'exit', 'ls', 'touch']
+  // use array.find
+  //
+  for (let index = 0; index < listOfCommands.length; index++) {
+    if (listOfCommands[index] === command) {
+      return listOfCallbacks[index](args);
+    }
+  }
+
+  return "no such command...";
+}
+
+const executeShell = function () {
+  while (continueToShell) {
+    const userCommand = prompt(shellPrompt + symbol);
+    if (!userCommand.trim()) continue;
+
+    const resultOfRunningCommand = runCommand(userCommand);
+    if (resultOfRunningCommand !== undefined) {
+      console.log(resultOfRunningCommand);
+    }
+  }
+}
+
+executeShell();
